6e4f571183dc5f6f6e7ec0199a01e83829243db4
crillion <crillion@tiscali.it>
2019-12-18 14:43:45 +0100

    Updates

---
+++ precache-manifest.2b6bd469cd5890048047586a04f51e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3411df544f25bcd4b72406a493f816",
+    "url": "/test_react/index.html"
+  },
+  {
+    "revision": "a089c67a3f8fc6b0712f",
+    "url": "/test_react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d56bc01c9f923646971b",
+    "url": "/test_react/static/js/2.d40500b0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test_react/static/js/2.d40500b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a089c67a3f8fc6b0712f",
+    "url": "/test_react/static/js/main.3185292b.chunk.js"
+  },
+  {
+    "revision": "275bcd96a6b1f2ff048b",
+    "url": "/test_react/static/js/runtime-main.3a1ba217.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
